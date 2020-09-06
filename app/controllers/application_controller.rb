51857831e634890b6fbe101f829bcb876b213abd
yuuki.tsujimura <snow1218kei@gmail.com>
2020-09-06 10:26:21 +0000

    task-finish

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
 
  include SessionsHelper
     
-  private
+ private
 
-  def require_user_logged_in
+ def require_user_logged_in
     unless logged_in?
       redirect_to login_url
     end
-  end
+ end
 end
